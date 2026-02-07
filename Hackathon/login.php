5a96e2de7664dd48e589c7a11749bcb033a9ac90
Isaac-Leeds <jbff0193@leeds.ac.uk>
2026-02-07 20:53:14 +0000

    Isaac

---
+++ Hackathon/login.php
@@ -0,0 +1,33 @@
+<?php
+$email = filter_input(INPUT_POST,"email", FILTER_SANITIZE_STRING);
+$password = filter_input(INPUT_POST,"password", FILTER_SANITIZE_STRING);
+
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "logindb";
+
+$conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+
+if ($conn->connect_error) {
+    die("Connect Error (". $conn->connect_errno .") ".$conn->connect_error);
+} 
+else 
+{
+    $stmt = ("SELECT Count(email) FROM logindetails WHERE email='".$email."' AND password='".$password."';");
+    if ($result = $conn -> query($stmt)) {
+        while($row = $result->fetch_assoc()) {
+            $resultOut = $row["Count(email)"];
+        }
+        if ($resultOut == '1') {
+            echo 'Succesful login';
+        }
+        elseif($resultOut == '0'){
+            echo 'Incorrect email or password';
+        }
+    } else {
+        echo "Error; ";
+    }
+    $conn->close();
+}
+?>
