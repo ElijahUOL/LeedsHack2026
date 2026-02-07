55b287f2d3631b1c206d3e5aa86b23585b63d03d
Elijah-Lu <ir0nw0lf13@outlook.com>
2026-02-07 20:51:54 +0000

    put in folder

---
+++ E's Work/index.js
@@ -0,0 +1,6 @@
+localStorage.setItem("name", "Elijah Luehrmann");
+localStorage.setItem("streak", 5);
+localStorage.setItem("lastAttended", "20/10/2024");
+localStorage.setItem("points", 100);
+localStorage.setItem("LectureCodesUsed", []);
+localStorage.setItem("purchased_items", []);
